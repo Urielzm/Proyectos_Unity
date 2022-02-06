3ffd65fddb7e1e9bf13b4459a43b7cc9ffd873f9
Uriel Zagoya <uriel-zagoya-mellado@hotmail.com>
2022-02-06 09:08:31 -0600

    Agregamos UI de vida y municiones

--- Tutorial01/Assets/Scripts/World/GameManager.cs
+++ Tutorial01/Assets/Scripts/World/GameManager.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text ammoText;
     
     public static GameManager Instance { get; private set; }
 
     public int gunAmmo = 10;
 
+    public Text HealtText;
+    public int healt = 100;
+
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Update()
+    {
+        ammoText.text = gunAmmo.ToString();
+        HealtText.text = healt.ToString();
     }
 
 }
